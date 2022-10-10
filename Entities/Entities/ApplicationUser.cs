6d78a88896536c495fb2db9f91e6b1997708ba80
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-10-09 22:14:47 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Enums;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column("Status_Usuario")]
+        public string CPF { get; set; }
+
+        [Column("Tipo_Usuario")]
+        public TipoUsuario? Tipo { get; set; }
+
+        [Column("UltimoAcesso_Usuario")]
+        public DateTime UltimoAcesso { get; set; }
+    }
+}
