6ed0c9dc15400f220b336d0f1a13a992b911d469
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-10-15 20:18:06 -0300

    Criação da Infrastructure Generice e Pessoa

---
+++ Infrastructure/Repository/Repositories/RepositoryPessoa.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Interfaces;
+using Entities.Entities.Pessoas;
+using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repository.Repositories
+{
+    public class RepositoryPessoa : RepositoryGenerics<Pessoa>, IPessoa
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+
+        public RepositoryPessoa()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+
+        public async Task<List<Pessoa>> ListarPessoa(Expression<Func<Pessoa, bool>> exPessoa)
+        {
+            using (var banco = new ContextBase(_OptionsBuilder))
+            {
+                return await banco.Pessoa.Where(exPessoa).AsNoTracking().ToListAsync();
+            }
+        }
+    }
+}
