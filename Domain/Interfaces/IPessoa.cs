6c8297c3115707c6b9b11aeb09d888af0d2889ca
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-10-15 19:41:30 -0300

    Criação do Domain Generic e Pessoa

---
+++ Domain/Interfaces/IPessoa.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Interfaces.Generics;
+using Entities.Entities.Pessoas;
+using System.Linq.Expressions;
+
+namespace Domain.Interfaces
+{
+    public interface IPessoa : IGeneric<Pessoa>
+    {
+        Task<List<Pessoa>> ListarPessoa(Expression<Func<Pessoa, bool>> exPessoa);
+    }
+}
---
+++ Domain/Interfaces/InterfaceServices/IServicePessoa.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Entities.Pessoas;
+
+namespace Domain.Interfaces.InterfaceServices
+{
+    public interface IServicePessoa
+    {
+        Task Adicionar(Pessoa Objeto);
+
+        Task Atualizar(Pessoa Objeto);
+
+        Task<List<Pessoa>> ListarPessoaAtivas();
+    }
+}
