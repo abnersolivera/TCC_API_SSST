e493eee226e67715bc97e24964570c5a1189b7f5
Abner Santos <abner.santos@bdsdatasolution.com.br>
2022-11-03 20:10:36 -0300

    Ajuste Nome

--- WebAPIs/Models/Login.cs
+++ WebAPIs/Models/Login.cs
@@ -2,7 +2,7 @@
 {
     public class Login
     {
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
     }
